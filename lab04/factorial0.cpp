bb76b56b497ed01de04f771661e367395831ebdb
Mohamed Emad Mahrous <123952213+3omdaughh@users.noreply.github.com>
2023-10-31 13:53:36 +0200

    Add files via upload

---
+++ lab04/factorial0.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <limits>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int num;
+    unsigned long long result = 1;
+
+    cout <<"Enter a number: ";
+    cin >>num;
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cerr << "Invalid Input.";
+        return 0;
+    }
+    if (num >= 65)
+    {
+        cout <<"MATH ERROR!";
+        return 0;
+    }
+
+    if (num < 0)
+    {
+        cout <<"MATH ERROR!";
+        return 0;
+    }
+    else if (num == 0)
+    {
+        cout <<"Factorial of "<< num <<" = " << num ;
+        return 0;
+    }
+
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+
+    cout <<"Factorial of "<< num <<" = " << result;
+}
